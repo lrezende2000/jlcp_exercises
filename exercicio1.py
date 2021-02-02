a34186960c564763c470d689a4b2ba3d2552da8d
LRezende <lucas.rezende.2000@gmail.com>
2021-02-02 20:52:56 -0300

    Exercicio 1

---
+++ exercicio1.py
@@ -0,0 +1,9 @@
+nome = "Lucas"
+idade = 21
+altura = 1.72
+atribuicoes = ['PHP', 'ReactJS', 'NodeJS']
+
+print(f'Nome: {nome}; Tipo: {type(nome)}')
+print(f'Idade: {idade}; Tipo: {type(idade)}')
+print(f'Altura: {altura}; Tipo: {type(altura)}')
+print(f'Atribuições: {", ".join(atribuicoes)}; Tipo: {type(atribuicoes)}')
